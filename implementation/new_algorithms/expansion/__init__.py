50ec26a338e7fc49aa08c5325fec9c22e440e75e
June Andrews <astuteajax@gmail.com>
2012-03-13 11:43:34 -0400

    updating inits

--- implementation/new_algorithms/expansion/__init__.py
+++ implementation/new_algorithms/expansion/__init__.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from candidates import *
+from community import *
 from grow import *
 
+import candidates
+import community
 import grow
--- implementation/testers/__init__.py
+++ implementation/testers/__init__.py
@@ -1,1 +1,6 @@
 # -*- coding: utf-8 -*-
+
+from test_arrangement import *
+from test_cliques import *
+from test_linearity import *
+from test_load_data import *
